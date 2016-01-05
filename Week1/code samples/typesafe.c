4abe10a9a5a254d3f5e7eb33ec1e3279ee0df287
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2016-01-05 14:28:54 -0500

    removed Fall files after moving them to archive

--- Week1/code samples/typesafe.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
- void foo();
-
- int main(void) {
-     foo(-25);
-}
- void foo(char x[]) {
-     printf("%s", x); /* ERROR */
-}
--- Week1/code samples/typesafe.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
- void foo(int x);
-
- int main(void) {
-     foo(-25);
-}
- void foo(int x) {
-     printf("%d\n", x); /* ERROR */
-}
